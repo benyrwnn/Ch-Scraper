11cedb1e11a4c4b7a925e0bd7744d6b7f10371d1
ScarLet <81684610+ScarLet0x@users.noreply.github.com>
2022-04-11 20:27:00 +0700

    🐈 Add komikcast

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 other? wait later
+
+if there is an error please chat me via whatsapp
+wa.me/6288286421519
---
+++ komikcast.js
@@ -0,0 +1,60 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const Base_URL = "https://komikcast.com";
+
+function Download(url) {
+  return new Promise((res, rej) => {
+    axios.get(url).then(({ data }) => {
+      let $ = cheerio.load(data)
+      let image = [];
+      let title = $('div.allc').find('a').text()
+      $('div.main-reading-area > img').get().map(s => {
+        image.push($(s).attr('src'))
+      })
+      res({ title, image })
+    }).catch(rej)
+  })
+}
+
+function Latest(page = 1) {
+  return new Promise((res, rej) => {
+    axios.get(`${Base_URL}/project-list/page/${page}/`).then(({ data }) => {
+      let $ = cheerio.load(data)
+      let result = [];
+      $('div.list-update_item').get().map(s => {
+        let title = $(s).find('h3').text()
+        let url = $(s).find('a').attr('href')
+        let type = $(s).find('span').text()
+        let thumb = $(s).find('img').attr('src')
+        let chapter = $(s).find('.chapter').text().trim().replace('Ch.', '').trim()
+        let rating = $(s).find('.numscore').text().trim()
+        result.push({ title, url, type, thumb, chapter, rating })
+      })
+      res(result)
+    }).catch(rej)
+  })
+}
+
+function Search(query) {
+  return new Promise((res, rej) => {
+    axios.get(`${Base_URL}/?s=${query}`).then(({ data }) => {
+      let $ = cheerio.load(data)
+      let result = [];
+      $('div.list-update_item').get().map(s => {
+        let title = $(s).find('h3').text()
+        let url = $(s).find('a').attr('href')
+        let type = $(s).find('span').text()
+        let thumb = $(s).find('img').attr('src')
+        let chapter = $(s).find('.chapter').text().trim().replace('Ch.', '').trim()
+        let rating = $(s).find('.numscore').text().trim()
+        result.push({ title, url, type, thumb, chapter, rating })
+      })
+      res(result)
+    }).catch(rej)
+  })
+}
+
+// Download("https://komikcast.com/chapter/drawing-saikyou-mangaka-wa-oekaki-skill-de-isekai-musou-suru-chapter-01-2-bahasa-indonesia/").then(console.log)
+// Latest(2).then(console.log)
+// Search("Yuan Zun").then(console.log)
